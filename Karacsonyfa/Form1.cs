ca7f5710d5d3f99ff78b84e4a3afa3e7d749bdbf
komarc <kozari.marton@gmail.com>
2024-02-29 15:03:43 +0100

    karacsonyfa

--- Karacsonyfa/Form1.cs
+++ Karacsonyfa/Form1.cs
@@ -9,7 +9,20 @@ namespace Karacsonyfa
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < 10; oszlop++)
+                {
+                    if  (sor <= oszlop)
+                    {
+                        Button gomb = new Button();
+                        Controls.Add(gomb);
+                        gomb.Size = new Size(20, 20);
+                        gomb.Left = ClientRectangle.Width / 2 + sor*20 - oszlop *20 / 2;
+                        gomb.Top = oszlop * 20;
+                    }
+                }
+            }
         }
     }
 }
